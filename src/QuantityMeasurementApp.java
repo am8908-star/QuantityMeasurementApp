c39aeb0937b0110dc5b168fbd65d47df97e51a7e
am8908-star <am8908@srmist.edu.in>
2026-04-24 14:15:45 +0530

    UC1

---
+++ src/QuantityMeasurementApp.java
@@ -0,0 +1,33 @@
+public class QuantityMeasurementApp {
+
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Feet feet = (Feet) obj;
+            return Double.compare(feet.value, value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+        System.out.println("Equal (" + f1.equals(f2) + ")");
+    }
+}
